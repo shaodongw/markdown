1b8eee9697ee164d93586c741f1f541421e2334a
Wang Shaodong <wsd@fujitsu>
2015-02-02 10:03:24 +0800

    first-subject is defined ready

--- mddata.rkt
+++ mddata.rkt
@@ -9,12 +9,42 @@
 (define-runtime-path test.md "./subject.markdown")
 (define xs (parse-markdown (file->string test.md #:mode 'text)
                           test-footnote-prefix))
-;;(pretty-print xs)
+
+(define (split-to-subjects xs)
+  (cond 
+    [(empty? xs) empty]
+    [(empty? (first-subject xs) empty)]
+    [else (cons (first-subject xs) (split-to-subjects (rest-subject xs)))]))
+
+(define (first-subject xs)
+  (define (fst-sbj xs subject)
+    (cond
+      [(empty? xs) empty]
+      [(is-h1? (second xs)) (cons (first xs) empty)]
+      [else (cons (first xs) (fst-sbj (rest xs) subject))]))
+  (fst-sbj xs empty))
+
+(define (rest-subject xs)
+  (cond
+    [(empty? xs) empty]
+    [(empty? (rest xs)) empty]
+    [(is-h1? (second xs)) (rest xs)]))
+
+(define (is-h1? piece)
+  (equal? (first piece) 'h1))
+
+(pretty-print (first-subject xs))
+;(pretty-print (rest-subject xs))
+
+
+
+
 
 ;; 2. Optionally, process the xexprs somehow:
 ;; ... nom nom nom ...
 
-xs
+;;xs
+
 
 ;; 3. Splice them into an HTML `xexpr` and...
 ;; 4. Convert to HTML text:
